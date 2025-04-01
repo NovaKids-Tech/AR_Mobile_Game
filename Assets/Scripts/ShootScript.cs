bb3fd0504c662a4b45970de3159a77ae1a9f02ea
Abdulkadir Erbas <akadir.erbass@gmail.com>
2025-04-02 00:39:37 +0300

    can gostergesi bugu duzeltilecek

---
+++ Assets/Scripts/ShootScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootScript : MonoBehaviour
+{
+    public GameObject arCamera;
+    public GameObject smoke;
+    private GameManager gameManager;
+
+    void Start()
+    {
+        // GameManager referansını al
+        gameManager = GameManager.Instance;
+        if (gameManager == null)
+        {
+            Debug.LogError("GameManager bulunamadı! Lütfen sahnede bir GameManager olduğundan emin olun.");
+        }
+    }
+
+    public void Shoot()
+    {
+        if (gameManager == null)
+        {
+            gameManager = GameManager.Instance;
+            if (gameManager == null)
+            {
+                Debug.LogError("GameManager hala bulunamadı!");
+                return;
+            }
+        }
+
+        RaycastHit hit;
+        if(Physics.Raycast(arCamera.transform.position, arCamera.transform.forward, out hit))
+        {
+            if(hit.transform.name == "balloon1(Clone)" || hit.transform.name == "balloon2(Clone)" || hit.transform.name == "balloon3(Clone)")
+            {
+                BalloonScript balloon = hit.transform.gameObject.GetComponent<BalloonScript>();
+                
+                if (balloon != null)
+                {
+                    if (balloon.isRedBalloon)
+                    {
+                        // Kırmızı balona vurulduğunda 50 puan düş
+                        gameManager.AddScore(-50);
+                    }
+                    else
+                    {
+                        // Normal balona vurulduğunda puan ekle
+                        gameManager.AddScore(balloon.pointValue);
+                    }
+                }
+
+                Destroy(hit.transform.gameObject);
+                Instantiate(smoke, hit.point, Quaternion.LookRotation(hit.normal));
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# AR-Balloon-Shooter
+
+List of supported devices (ARCore): https://developers.google.com/ar/discover/supported-devices
+
+Just download the .apk file on your device, and enjoy the game.
+
+## Screenshots
+
+![image](screenshots/1.jpeg)
+![image](screenshots/2.jpeg)
+![image](screenshots/3.jpeg)
