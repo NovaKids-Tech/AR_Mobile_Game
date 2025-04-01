c41a404e07816315d96a14cd65d8f6df5a275b7b
Abdulkadir Erbas <akadir.erbass@gmail.com>
2025-04-02 01:03:31 +0300

    yeniden basla bugu duzeltilecek

--- Assets/Scripts/BalloonScript.cs
+++ Assets/Scripts/BalloonScript.cs
@@ -7,6 +7,7 @@ public class BalloonScript : MonoBehaviour
     public float lifetime = 7f;
     public int pointValue = 10; // Varsayılan değer sarı balon için
     public bool isRedBalloon = false;
+    private bool wasShot = false; // Balonun vurulup vurulmadığını takip etmek için
 
     private void Start()
     {
@@ -20,10 +21,16 @@ public class BalloonScript : MonoBehaviour
         transform.Translate(Vector3.up * Time.deltaTime * 0.2f);
     }
 
+    public void Shot()
+    {
+        wasShot = true;
+        Destroy(gameObject);
+    }
+
     private void OnDestroy()
     {
         // Eğer balon vurulmadan yok olduysa (yaşam süresi dolduysa)
-        if (!isRedBalloon)
+        if (!wasShot && !isRedBalloon)
         {
             GameManager.Instance.LoseLife();
         }
